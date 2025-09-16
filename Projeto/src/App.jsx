ce750e812684fa5bf6160625205e0a690741f392
AlexandreCalore~ <caloredev@gmail.com~>
2025-09-16 13:00:41 -0300

    Estudos React

--- Projeto/src/App.jsx
+++ Projeto/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrouseRouter, Routes, Route } from "react-router-dom" // Instalado componentes e importados para trabalharmos como 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+import Titulo from './Titulo.jsx' // Aqui, criamos um componente personalizado, para trabalhar com ele como se fosse uma tag, contendo dentro dele outras tags específicas de acordo com a nossa necessidade
+import Home from "./pages/Home.jsx";
+import Sobre from "./pages/Sobre.jsx";
+import Contato from "./pages/Contato.jsx";
+
+
+function App(){ //Retonarmos por meio de função, tudo aquilo que será incorporado no HTML -> na sua class'root'.
+  return ( //Aqui utilizei aquele componente criado pos nós mesmos, em forma de tag, exibindo na tela tudo o que nele possuí. 
+
+    // Se formos utiliza-lo mais de uma vez, deve-se então usar esses comandos conforme abaixo: 
+    <div>
+      <BrousetRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Sobre' element={<Sobre />} />
+          <Route path='/Contato' element={<Contato />} />
+        </Routes>
+      </BrousetRouter>
+      <Home />
+      <Titulo nomeProps={"Alexandre"} />
+      <Titulo nomeProps={"Calore"}/> 
+      <Titulo nomeProps={"Props"}/> 
+      <Titulo/> 
+    </div> 
+
+    // Do contrário, somente printar uma tag
+    //<Titulo/>
+
+    //Podemos também trabalhar com parâmetros + if/else com estes parâmetros. No caso do titulo, podemos definir o nome a aparecer, e no caso de nao aparecer, trabalhando com if/else, ele vai printar outra coisa.
   )
-}
+} 
 
-export default App
+export default App //Necessário para exporta-lo para a main
---
+++ Projeto/src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu() {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/Sobre">Sobre</a>
+            <a href="/Contato">Contato</a>
+        </div>
+    )
+}
+
+export default Menu
